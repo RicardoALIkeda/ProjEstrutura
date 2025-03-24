93ea64ab59c777aad3e867247c842552f60a5512
Diego Estevão Lopes de Queiroz <deadzone8727@gmail.com>
2025-03-24 13:25:55 +0000

    aumentar capacidade inicial da lista de países

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private static List<Pais> paises = new ArrayList<>();
+    private static List<Pais> paises = new ArrayList<>(100);
     private static boolean dadosCarregados = false;
 
     public static void main(String[] args) {
